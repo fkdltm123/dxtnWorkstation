fed83886dd5d41b8b639c1b0331822c4722236e3
bongkyunbyun <bbk1072@gmail.com>
2024-02-13 19:12:14 +0900

    board first

---
+++ src/main/java/com/dxtn/erp/repository/boardRepository.java
@@ -0,0 +1,9 @@
+package com.dxtn.erp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dxtn.erp.entity.boardEntity;
+
+public interface boardRepository extends JpaRepository<boardEntity, Integer>{
+
+}
